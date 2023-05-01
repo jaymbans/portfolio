3bab6dba9523c3f4b531a462b51e55ec8a3300a6
jay mbans <jordan@mbanefo.com>
2023-05-01 18:34:39 -0400

    about me page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <LandingPage />
+      <AboutMe />
     </div>
   );
 }
---
+++ portfolio/src/components/AboutMe.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+function AboutMe() {
+  return (
+    <section id='about-me'>
+      <h1>about me</h1>
+      <div className="content">
+        <div>
+          <h3>
+            thanks for tuning in to my elevator pitch
+          </h3>
+          <p>
+            I’m a software engineer, currently working as a technology consultant at <a target='_blank' href='https://www2.deloitte.com/us/en.html' className='external-link'>Deloitte</a>. My role has ranged from eCommerce <span className='bolded'>web development</span>, to <span className='bolded'>data analytics</span> and <span className='bolded'>engineering</span>. Though consulting has made me a bit nomadic, I am based out of New York City 📍. Prior to this role, I studied accounting (hear me out, I can explain...) and information systems at <a target='_blank' href='https://www1.villanova.edu/university.html' className='external-link'>Villanova University</a>.
+          </p>
+          <br />
+          <p>
+            I love programming, and have grown a passion for software  development. Switching from finance to tech has given me a unique perspective on the intersection between business and digital adoption. I am <span className='bolded'>front-end</span> focused, with <span className='bolded'>back-end</span> experience.
+          </p>
+        </div>
+
+        <div>
+          <div className="fun-facts">
+            <p>
+              <span className='desc'>
+                Name:
+              </span>Jordan
+            </p>
+            <p>
+              <span className='desc'>
+                Age:
+              </span>23
+            </p>
+            <p>
+              <span className='desc'>
+                From:
+              </span>New York, New York
+            </p>
+            <p>
+              <span className='desc'>
+                Favorite Sports Team:
+              </span>The New York Knicks
+            </p>
+            <p>
+              <span className='desc'>
+                Email:
+              </span>
+              <a className='bolded' href="mailto: jordan@mbanefo.com">
+                jordan@mbanefo.com
+              </a>
+            </p>
+          </div>
+          <div className="socials">
+            <a target='_blank' href="https://www.linkedin.com/in/jordan-mbanefo-80918916b/">
+              <img src={require('../assets/linkedin-logo.png')} />
+            </a>
+            <a target='_blank' href='https://github.com/jaymbans'>
+              <img src={require('../assets/github-logo.png')} />
+            </a>
+          </div>
+          <button className="blue-btn">
+            Resume
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutMe
